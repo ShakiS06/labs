cc4669fbc9ef323b0f9e5e94ff0d76e866268675
ShakiS06 <ns645455@gmail.com>
2024-09-29 12:28:26 +0300

    Add files via upload

---
+++ Лаба№2задание1.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print('Здравствуйте,', name)
+greet('Иванов Иван')
+
+def square(number):
+    return print(number ** 2)
+square(9)
+
+def max_of_two(x, y):
+    if x > y: return x
+    if y > x: return y
+print(max_of_two(10, 11))
