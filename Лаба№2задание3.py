537ea5dc29189417d9d856d04d1f2e40ba0152f5
ShakiS06 <ns645455@gmail.com>
2024-09-29 12:38:53 +0300

    Add files via upload

---
+++ Лаба№2задание3.py
@@ -0,0 +1,8 @@
+#Задание 3: Использование функций для решения алгоритмических задач
+def is_prime(number):
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+        else:
+            return True
+print(is_prime(77))
